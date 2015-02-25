c1f2b75dd6603e8c530e9505f98238532a30a4a6
Artem Nikitin <artemnikitin@artemnikitin.com>
2015-02-25 21:30:39 +0300

    add config

--- src/main/java/lazada/Config.java
+++ src/main/java/lazada/Config.java
@@ -12,4 +12,24 @@ public class Config {
         return config.getString("selenium-tests.lazada.url");
     }
 
+    public static String getUserName() {
+        return config.getString("selenium-tests.lazada.user.name");
+    }
+
+    public static String getUserEmail() {
+        return config.getString("selenium-tests.lazada.user.email");
+    }
+
+    public static String getUserPassword() {
+        return config.getString("selenium-tests.lazada.user.password");
+    }
+
+    public static String getProduct1Page() {
+        return config.getString("selenium-tests.lazada.product1");
+    }
+
+    public static String getProduct2Page() {
+        return config.getString("selenium-tests.lazada.product2");
+    }
+
 }
